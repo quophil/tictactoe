9f7f2a3bbcc21f94ec1a01e435b80d8e13d26e5b
Phillip Zeba <phillip.zeba@gmx.at>
2021-07-27 11:26:40 +0200

    updated the code to be flexible for grid expansion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import system
+import math
 
 #function for clearing the terminal, displaying certain strings and for resetting the grid to its default state
 
@@ -44,15 +45,9 @@ class Field():
 #function for updating the value of each Field
 
 def field_update():
-    Field_1.value = tictactoe_grid[Field_1.index]
-    Field_2.value = tictactoe_grid[Field_2.index]
-    Field_3.value = tictactoe_grid[Field_3.index]
-    Field_4.value = tictactoe_grid[Field_4.index]
-    Field_5.value = tictactoe_grid[Field_5.index]
-    Field_6.value = tictactoe_grid[Field_6.index]
-    Field_7.value = tictactoe_grid[Field_7.index]
-    Field_8.value = tictactoe_grid[Field_8.index]
-    Field_9.value = tictactoe_grid[Field_9.index]
+    for i in field_list:
+        i.value = tictactoe_grid[i.index]
+
 
 #function for resetting most of the game variables to their default state
 
@@ -64,7 +59,8 @@ def game_reset():
     chosen_numbers = []
     global active_player
     active_player = Player_1
-    field_update
+    global valid_numbers
+    valid_numbers = []
 
 #function for displaying the winner screen
 
@@ -79,7 +75,7 @@ def postwin_print(num):
 quitstatus = True
 Player_1 = Player('Player 1', 'X')
 Player_2 = Player('Player 2', 'O')
-valid_numbers = ['1','2', '3', '4', '5', '6', '7', '8', '9']
+
 
 #main menu
 title_display()
@@ -96,92 +92,72 @@ while quitstatus:
         Field_7 = Field(7, 137)
         Field_8 = Field(8, 143)
         Field_9 = Field(9, 149)
+        field_list = [Field_1, Field_2, Field_3, Field_4, Field_5, Field_6, Field_7,Field_8, Field_9]
+        grid_width = int(math.sqrt(len(field_list)))
+        for i in range(1, (len(field_list)+1)):
+            valid_numbers.append(str(i))
         grid_display()
-#menu once in game
+# menu once in game 
         while gamestatus:
 #win conditions
-            if Field_1.value == 'X' and Field_2.value == 'X' and Field_3.value == 'X':
-                postwin_print(1)
+            horizontal_counter = 0
+            for index, field  in enumerate(field_list):
+                while (index + 1) > horizontal_counter:
+                    horizontal_counter += grid_width
+                if field.value == 'X':
+                    if horizontal_counter - (index + 1) >= 2 and field_list[index + 1].value == 'X' and field_list[index + 2].value == 'X':
+                        postwin_print(1)
+                    elif (index + 1) <= (len(field_list)) - (2*grid_width) and field_list[index + grid_width + grid_width].value == 'X' and field_list[index + grid_width].value == 'X':
+                        postwin_print(1)
+                    elif grid_width - (index + 1) >= 2 and field_list[index + (grid_width + 1)].value == 'X' and field_list[(index + (grid_width + 1)) + (grid_width + 1)].value == 'X':
+                        postwin_print(1)
+                elif field.value == 'O':
+                    if horizontal_counter - (index + 1) >= 2 and field_list[index + 1].value == 'O' and field_list[index + 2].value == 'O':
+                        postwin_print(2)
+                    elif (index + 1) <= (len(field_list)) - (2*grid_width) and field_list[index + grid_width + grid_width].value == 'O' and field_list[index + grid_width].value == 'O':
+                        postwin_print(2)
+                    elif grid_width - (index + 1) >= 2 and field_list[index + (grid_width + 1)].value == 'O' and field_list[(index + (grid_width + 1)) + (grid_width + 1)].value == 'O':
+                        postwin_print(2)
 
-            elif Field_1.value == 'X' and Field_4.value == 'X' and Field_7.value == 'X':
-                postwin_print(1)
-
-            elif Field_1.value == 'X' and Field_5.value == 'X' and Field_9.value == 'X':
-                postwin_print(1)
-
-            elif Field_2.value == 'X' and Field_5.value == 'X' and Field_8.value == 'X':
-                postwin_print(1)
-
-            elif Field_1.value == 'X' and Field_5.value == 'X' and Field_9.value == 'X':
-                postwin_print(1)
-
-            elif Field_3.value == 'X' and Field_5.value == 'X' and Field_7.value == 'X':
-                postwin_print(1)
-
-            elif Field_3.value == 'X' and Field_6.value == 'X' and Field_9.value == 'X':
-                postwin_print(1)
-
-            elif Field_4.value == 'X' and Field_5.value == 'X' and Field_6.value == 'X':
-                postwin_print(1)
-            
-            elif Field_1.value == 'O' and Field_2.value == 'O' and Field_3.value == 'O':
-                postwin_print(2)
-                    
-            elif Field_1.value == 'O' and Field_4.value == 'O' and Field_7.value == 'O':
-                postwin_print(2)
-
-            elif Field_1.value == 'O' and Field_5.value == 'O' and Field_9.value == 'O':
-                postwin_print(2)
-
-            elif Field_2.value == 'O' and Field_5.value == 'O' and Field_8.value == 'O':
-                postwin_print(2)
-
-            elif Field_1.value == 'O' and Field_5.value == 'O' and Field_9.value == 'O':
-                postwin_print(2)
-
-            elif Field_3.value == 'O' and Field_5.value == 'O' and Field_7.value == 'O':
-                postwin_print(2)
-
-            elif Field_3.value == 'O' and Field_6.value == 'O' and Field_9.value == 'O':
-                postwin_print(2)
-
-            elif Field_4.value == 'O' and Field_5.value == 'O' and Field_6.value == 'O':
-                postwin_print(2)
 #draw condition
-            elif str(Field_1.pos) != Field_1.value and str(Field_2.pos) != Field_2.value and str(Field_3.pos) != Field_3.value and str(Field_4.pos) != Field_4.value and str(Field_5.pos) != Field_5.value and str(Field_6.pos) != Field_6.value and str(Field_7.pos) != Field_7.value and str(Field_8.pos) != Field_8.value and str(Field_9.pos) != Field_9.value:
-                title_display()
-                print(tictactoe_grid)
-                print('\n\tIt`s a draw!')
-                gamestatus = False
-                
-#player input checker
-            else:
-                chosen_num_index = ''
-                number_input = input(f"\n\n{active_player.name}, input the number of the field you want to mark or type 'q' to Quit: ")
-                if number_input in chosen_numbers:
-                    grid_display()
-                    print('\n\nPlease enter a number that has not been taken already')
-
-                elif number_input in valid_numbers:
-                    chosen_num_index = tictactoe_grid.find(number_input)
-                    tictactoe_grid = tictactoe_grid[:chosen_num_index] + active_player.symbol + tictactoe_grid[chosen_num_index+1:]
-                    chosen_numbers.append(number_input)
-                    field_update()
-                    if active_player == Player_1:
-                        active_player = Player_2
-
-                    elif active_player == Player_2:
-                        active_player = Player_1
-                    
-                    grid_display()
-     
-                elif number_input == 'q':
-                    gamestatus = False
+            draw_counter = 0
+            for i in field_list:
+                if i.value not in valid_numbers:
+                    draw_counter += 1
+                elif draw_counter == len(field_list):
                     title_display()
+                    print(tictactoe_grid)
+                    print('\n\tIt`s a draw!')
+                    gamestatus = False
 
-                else:
-                    grid_display()
-                    print('\n\nPlease enter a valid input')
+#player input checker 
+            if gamestatus:
+                    chosen_num_index = ''
+                    number_input = input(f"\n\n{active_player.name}, input the number of the field you want to mark or type 'q' to Quit: ")
+                    if number_input in chosen_numbers:
+                        grid_display()
+                        print('\n\nPlease enter a number that has not been taken already')
+
+                    elif number_input in valid_numbers:
+                        chosen_num_index = tictactoe_grid.find(number_input)
+                        tictactoe_grid = tictactoe_grid[:chosen_num_index] + active_player.symbol + tictactoe_grid[chosen_num_index+1:]
+                        chosen_numbers.append(number_input)
+                        field_update()
+                        if active_player == Player_1:
+                            active_player = Player_2
+
+                        elif active_player == Player_2:
+                            active_player = Player_1
+                            
+                        grid_display()
+            
+                    elif number_input == 'q':
+                        gamestatus = False
+                        title_display()
+
+                    else:
+                        grid_display()
+                        print('\n\nPlease enter a valid input')
 
     elif start_question == 'q':
         quitstatus = False
